0056c344710852f9070be3a730929ad13fa848d9
Souleymane-diakite <sd797@njit.edu>
2021-04-12 10:04:10 -0400

    added websocket stuff

---
+++ Docker/docker-demo/websocket/app.js
@@ -0,0 +1,72 @@
+
+'use strict';
+const e = React.createElement;
+
+class Messages extends React.Component {
+	messageEvent(event) {
+		this.setState({messages: this.state.messages.concat(event.data)});
+	        }
+		constructor(props) {
+			super(props);
+			this.state = {messages: []};
+	                props.socket.addEventListener('message', this.messageEvent.bind(this));
+	        }
+
+	        render() {
+	            return this.state.messages.map(
+	                (message) => e('div', null, `${message}`)
+		)
+	}
+}
+
+class Send extends React.Component {
+	constructor(props) {
+		super(props);
+	        this.state = {action: '', data: ''};
+	        this.changeAction = this.changeAction.bind(this);
+	        this.changeData = this.changeData.bind(this);
+	        this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	changeAction(event) {
+		this.setState({action: event.target.value, data: this.state.data});
+	}
+
+	changeData(event) {
+		this.setState({action: this.state.action, data: event.target.value});
+	}
+
+	handleSubmit(event) {
+		this.props.socket.send(
+			JSON.stringify({action: this.state.action, data: this.state.data}));
+		this.setState({action: '', data: ''});
+	        event.preventDefault();
+	}
+
+	render() {
+		return e("form", {onSubmit: this.handleSubmit},
+			e("label", null, "Action:",
+				e("input", {type: "text", value: this.state.action,
+					onChange: this.changeAction})),
+			e("label", null, "Data:",
+				e("input", {type: "text", value: this.state.data,
+					onChange: this.changeData})),
+			e("input", {type: "submit", value: "Send"}));
+	}
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {socket: new WebSocket('ws://localhost:8080')}
+	}
+
+	render() {
+		return [
+			e(Messages, {socket: this.state.socket}),
+	                e(Send, {socket: this.state.socket})
+		]
+	}
+}
+
+ReactDOM.render(e(App), document.getElementById('root'));
---
+++ Docker/docker-demo/websocket/server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Messaging configuration
+const amqp = require('amqplib');
+const messaging_host = 'messaging';
+const messaging_user = process.env.MSG_USER;
+const messaging_pass = process.env.MSG_PASS;
+const queue = 'requests';
+const messaging_url = `amqp://${messaging_user}:${messaging_pass}@${messaging_host}`;
+// // use one connection for all AMPQ things
+var messaging_conn = amqp.connect(messaging_url);
+
+ // Express configuration
+const express = require('express');
+const app = express();
+const port = 8080;
+// serve static files from the current dir
+app.use(express.static(__dirname))
+const server = app.listen(port);
+//
+// ws configuration
+const ws = require('ws');
+const wsServer = new ws.Server({ noServer: true });
+//
+// since we are sharing a server we pass off upgrade requests to ws
+server.on('upgrade', (request, socket, head) => {
+	wsServer.handleUpgrade(request, socket, head, socket => {
+		wsServer.emit('connection', socket, request);
+	});
+});
+ // Set up a WebSocket AMPQ proxy
+wsServer.on('connection', function(socket) {
+	console.log("WebSocket connection established");
+	messaging_conn.then(function(conn) {
+		console.log("Building AMQP channel");
+		conn.createChannel().then(function(ch) {
+			console.log("Creating AMPQ response queue");
+                        ch.assertQueue('', { exclusive: true }).then(function(ok) {
+				console.log("Creating callback for AMPQ response messages");
+				ch.consume(ok.queue, function(response) {
+					console.log(`AMQP ${ok.queue} -> WebSocket`);
+					socket.send(response.content.toString());
+				}, { noAck: true });
+				return ok;
+			}).then(function((ok) {
+				console.log("Creating a callback for WebSocket messages");                                  socket.on('message', function(message) {
+					console.log(`WebSocket -> AMQP ${queue}`);
+                                        ch.sendToQueue(queue, Buffer.from(message),
+						{ replyTo: ok.queue });
+				});
+			});
+		});
+	});
+});                                                                                           console.log("Serving static files and listening for WebSocket connections...");
